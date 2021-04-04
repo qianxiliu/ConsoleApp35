cd255d2c4d7a6c072b15378465368a28fbb986cf
Qianxi Liu <qianxiliu@usf.edu>
2021-04-04 14:26:36 -0400

    Database

--- ConsoleApp35/Program.cs
+++ ConsoleApp35/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConsoleApp35
 {
+
+
+    class Store
+    {
+        public int StoreID { get; set; }
+
+        public Product StoreProduct { get; set; }
+
+        public string location { get; set; }
+
+        public List<Product> Products { get; set; }
+
+    }
+
+    class Product
+    {
+        public int ProductID { get; set; }
+
+        public Order OrderID { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Order> Orders { get; set; }
+
+
+    }
+
+    class Order
+    {
+        public int OrderID { get; set; }
+
+        public Product ProductID { get; set; }
+
+        public string Information { get; set; }
+
+        public List<Product> Products { get; set; }
+
+    }
+
+    class ProductContext : DbContext
+    {
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        string connectionString = "Server=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (ProductContext context = new ProductContext())
+            {
+                context.Database.EnsureCreated();
+            }
         }
     }
 }
